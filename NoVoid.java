968e67b068b073016d85c94c3b3e2715560eb060
Donovan Duperrois <slashhable@gmail.com>
2023-02-05 18:43:01 +0100

    first commit

---
+++ NoVoid.java
@@ -0,0 +1,29 @@
+
+
+
+
+
+public class Main extends JavaPlugin implements Listener {
+    private String worldName;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        FileConfiguration config = getConfig();
+        config.addDefault("world", "world");
+        config.options().copyDefaults(true);
+        saveConfig();
+        worldName = config.getString("world");
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location loc = player.getLocation();
+        World world = player.getWorld();
+
+        if (world.getName().equals(worldName) && loc.getY() < 0) {
+            player.teleport(world.getSpawnLocation());
+        }
+    }
+}
